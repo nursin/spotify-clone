f80fd15bcc54d4286177090065e6d54664208418
Bobby Keel <bobbykeel76@gmail.com>
2021-11-25 03:16:56 -0600

    added user playlist, js map user playlist and added to datalayer

--- src/reducer.js
+++ src/reducer.js
@@ -4,23 +4,29 @@ export const initialState = {
     playing: false,
     item: null,
     // remove after finish developing
-    token: 'BQBwnOC19O0csILQMq8DyXdDGTJka5YU2C9lr5cjsxNH_Jlc_9Sq-ezjFBcqJkz97P7lKwDD7mcpJ_eq12PB1GAX6EqwY8J4LvRNopgmjPJkJdy0Ji_gptKIWS5S_cEQcN9-rJORmLsV6r6d6G3LNhLtTier1u78qg',
+    // token: 'BQBwnOC19O0csILQMq8DyXdDGTJka5YU2C9lr5cjsxNH_Jlc_9Sq-ezjFBcqJkz97P7lKwDD7mcpJ_eq12PB1GAX6EqwY8J4LvRNopgmjPJkJdy0Ji_gptKIWS5S_cEQcN9-rJORmLsV6r6d6G3LNhLtTier1u78qg',
 };
 
 export const reducer = (state, action) => {
     console.log(action);
 
-    switch(action.type) {
+    switch (action.type) {
         case 'SET_USER':
             return {
-                ...state, 
+                ...state,
                 user: action.user,
             };
-        
+
         case 'SET_TOKEN':
             return {
                 ...state,
                 token: action.token,
+            };
+
+        case 'SET_PLAYLISTS':
+            return {
+                ...state,
+                playlists: action.playlists,
             };
 
         default:
