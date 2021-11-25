06b55242de199804c1e084e5fc89bce2e0fc5e9c
Bobby Keel <bobbykeel76@gmail.com>
2021-11-25 08:33:27 -0600

    removed token

--- src/reducer.js
+++ src/reducer.js
@@ -3,8 +3,6 @@ export const initialState = {
     playlists: [],
     playing: false,
     item: null,
-    // remove after finish developing
-    // token: 'BQBwnOC19O0csILQMq8DyXdDGTJka5YU2C9lr5cjsxNH_Jlc_9Sq-ezjFBcqJkz97P7lKwDD7mcpJ_eq12PB1GAX6EqwY8J4LvRNopgmjPJkJdy0Ji_gptKIWS5S_cEQcN9-rJORmLsV6r6d6G3LNhLtTier1u78qg',
 };
 
 export const reducer = (state, action) => {
