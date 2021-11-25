588dfdcd5a3eda6c1c24e25358a72e9753426c79
Bobby Keel <bobbykeel76@gmail.com>
2021-11-25 04:58:49 -0600

    footer complete

--- src/Footer.js
+++ src/Footer.js
@@ -1,10 +1,48 @@
 import React from 'react';
 import './Footer.css';
+import PlayCircleOutlineIcon from '@mui/icons-material/PlayCircleOutline';
+import SkipPreviousIcon from '@mui/icons-material/SkipPrevious';
+import SkipNextIcon from '@mui/icons-material/SkipNext';
+import ShuffleIcon from '@mui/icons-material/Shuffle';
+import RepeatIcon from '@mui/icons-material/Repeat';
+import {Grid, Slider} from '@mui/material';
+import PlaylistPlayIcon from '@mui/icons-material/PlaylistPlay';
+import VolumeDownIcon from '@mui/icons-material/VolumeDown';
 
 function Footer() {
     return (
         <div className="footer">
-            <h1>I am footer</h1>
+            <div className="footer__left">
+                <img className="footer__albumLogo" src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" alt="" />
+                <div className="footer__songInfo">
+                    <h4>Yeah!</h4>
+                    <p>Usher</p>
+                </div>
+            </div>
+
+            <div className="footer__center">
+                <ShuffleIcon className="footer__green" />
+                <SkipPreviousIcon className="footer__icon" />
+                <PlayCircleOutlineIcon className="large" />
+                <SkipNextIcon className="footer__icon" />
+                <RepeatIcon className="footer__green" />
+
+
+            </div>
+
+            <div className="footer__right">
+                <Grid container spacing={2}>
+                    <Grid item>
+                        <PlaylistPlayIcon />
+                    </Grid>
+                    <Grid item>
+                        <VolumeDownIcon />
+                    </Grid>
+                    <Grid item xs>
+                        <Slider />
+                    </Grid>
+                </Grid>
+            </div>
         </div>
     )
 }
--- src/reducer.js
+++ src/reducer.js
@@ -4,7 +4,7 @@ export const initialState = {
     playing: false,
     item: null,
     // remove after finish developing
-    // token: 'BQBwnOC19O0csILQMq8DyXdDGTJka5YU2C9lr5cjsxNH_Jlc_9Sq-ezjFBcqJkz97P7lKwDD7mcpJ_eq12PB1GAX6EqwY8J4LvRNopgmjPJkJdy0Ji_gptKIWS5S_cEQcN9-rJORmLsV6r6d6G3LNhLtTier1u78qg',
+    token: 'BQBwnOC19O0csILQMq8DyXdDGTJka5YU2C9lr5cjsxNH_Jlc_9Sq-ezjFBcqJkz97P7lKwDD7mcpJ_eq12PB1GAX6EqwY8J4LvRNopgmjPJkJdy0Ji_gptKIWS5S_cEQcN9-rJORmLsV6r6d6G3LNhLtTier1u78qg',
 };
 
 export const reducer = (state, action) => {
